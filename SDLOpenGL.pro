fe0a73a917eae488b6d43c92c4ea6cac5d07e884
REmery <i7679227@bournemouth.ac.uk>
2016-02-22 11:38:59 +0000

    everything basic working

--- SDLOpenGL.pro
+++ SDLOpenGL.pro
@@ -1,10 +1,15 @@
 TARGET=SDLOpenGL
 
 OBJECTS_DIR=obj
+INCLUDEPATH+=$$PWD/include
 
 SOURCES+=$$PWD/src/main.cpp \
          $$PWD/src/SDLOpenGL.cpp
 
 HEADERS+=$$PWD/include/SDLOpenGL.h
+
+#ading SDL2 libraries
+QMAKE_CXXFLAGS += $$system(sdl2-config --cflags)
+LIBS +=$$system(sdl2-config --libs)
 
 include($(HOME)/NGL/UseNGL.pri)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "SDLOpenGL.h"
+
+#include<ngl/NGLInit.h>
+
+void intializeGL();
+void renderGL();
 
 int main (int argc, char *argv[])
 {
+  //calls SDLOpenGL class constructor
+  SDLOpenGL window("test",0,0,1024,720);
+  window.makeCurrent();
+  intializeGL();
+  while (true)
+  {
+    renderGL;
+    window.swapWindow();
+    SDL_Delay(10);
+  }
   std::cout << "working\n";
+}
+
+void intializeGL()
+{
+  ngl::NGLInit::instance();
+  glClearColor(0.8,0.8,0.8,1.0);
+}
+
+void renderGL()
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
